6dd302e220361e52dae654e7096dac6c8750f05a
Bryan <bryan.prolong@outlook.fr>
2025-04-13 10:49:55 +0200

    feat: first render of gif from canvas

--- app/controllers/renderer_controller.ts
+++ app/controllers/renderer_controller.ts
@@ -1,9 +1,12 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import app from '@adonisjs/core/services/app'
-import readlineiter from 'readlineiter'
 import { Fonts, Gif, Renderer } from '@matrixled-ssr/renderer'
 import { createCanvas } from 'canvas'
-import { readFile } from 'node:fs/promises'
+import ffmpeg from 'fluent-ffmpeg'
+import { mkdtemp, readFile, rmdir, writeFile } from 'node:fs/promises'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
+import readlineiter from 'readlineiter'
 
 function getDisplayTime() {
   const now = new Date()
@@ -11,26 +14,59 @@ function getDisplayTime() {
 }
 
 export default class RendererController {
-  public async render({ response, request }: HttpContext) {
-    const qs = request.qs()
-
-    const frame = qs.frame ?? 0
-
+  public async render({ response }: HttpContext) {
     const fonts = new Fonts(readlineiter)
     const gif = await readFile(app.publicPath('pacman-small.gif'))
+    const font = await fonts.get(app.publicPath('bitbuntu-full.bdf'))
 
     const canvas = createCanvas(64, 32)
 
     const renderer = new Renderer(canvas, fonts, (width, height) => createCanvas(width, height))
     const gifComponent = new Gif(renderer, gif.buffer)
 
-    for (let i = 0; i <= frame; i++) {
+    const processDir = await mkdtemp(join(tmpdir(), 'frames-'))
+
+    const renders = []
+
+    for (let i = 0; i < gifComponent.frames.length; i++) {
       renderer.clear()
-      gifComponent.render(frame)
-      renderer.writeNativeText(getDisplayTime(), 17, 1, {})
+      gifComponent.render(i)
+      renderer.drawBitmapText(font, getDisplayTime(), 17, 1, {
+        color: '#ffffff',
+      })
+
+      const imageData = canvas.toBuffer('image/png')
+      renders.push(
+        new Promise((resolve) => {
+          writeFile(join(processDir, `frame-${i}.png`), imageData).then(() => {
+            resolve(true)
+          })
+        })
+      )
     }
 
-    response.safeHeader('Content-Type', 'image/png')
-    return response.stream(canvas.createPNGStream())
+    await Promise.all(renders)
+
+    const g = ffmpeg()
+      .input(join(processDir, 'frame-%d.png'))
+      .output(join(processDir, 'output.gif'))
+
+    const buffer = await new Promise<Buffer>((resolve, reject) => {
+      g.on('end', async () => {
+        readFile(join(processDir, 'output.gif')).then((data) => {
+          resolve(data)
+        })
+      })
+        .on('error', (err) => {
+          console.error('Error:', err)
+          reject(err)
+        })
+        .run()
+    })
+
+    await rmdir(processDir, { recursive: true })
+
+    response.safeHeader('Content-Type', 'image/gif')
+    response.send(buffer)
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -7,78 +7,61 @@
 |
 */
 
-import router from "@adonisjs/core/services/router";
-import { middleware } from "./kernel.js";
-import transmit from "@adonisjs/transmit/services/main";
+import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
+import transmit from '@adonisjs/transmit/services/main'
 
-router.get("renderer", [
-  () => import("#controllers/renderer_controller"),
-  "render",
-]);
+router.get('renderer', [() => import('#controllers/renderer_controller'), 'render'])
 
 transmit.registerRoutes((route) => {
-  route.middleware(middleware.auth());
-});
+  route.middleware(middleware.auth())
+})
 
 router
-  .get("", [() => import("#controllers/home_controller"), "index"])
-  .as("home")
-  .use(middleware.auth());
+  .get('', [() => import('#controllers/home_controller'), 'index'])
+  .as('home')
+  .use(middleware.auth())
 
-const LoginController = () => import("#controllers/auth/login_controller");
-router
-  .resource("auth/login", LoginController)
-  .only(["index", "store"])
-  .use("*", middleware.guest());
+const LoginController = () => import('#controllers/auth/login_controller')
+router.resource('auth/login', LoginController).only(['index', 'store']).use('*', middleware.guest())
 
-const LogoutController = () => import("#controllers/auth/logout_controller");
-router
-  .resource("auth/logout", LogoutController)
-  .only(["store"])
-  .use("*", middleware.auth());
+const LogoutController = () => import('#controllers/auth/logout_controller')
+router.resource('auth/logout', LogoutController).only(['store']).use('*', middleware.auth())
 
-const RegisterController = () =>
-  import("#controllers/auth/register_controller");
+const RegisterController = () => import('#controllers/auth/register_controller')
 router
-  .resource("auth/register", RegisterController)
-  .only(["index", "store"])
-  .use("*", middleware.guest());
+  .resource('auth/register', RegisterController)
+  .only(['index', 'store'])
+  .use('*', middleware.guest())
 
-const PasswordResetController = () =>
-  import("#controllers/auth/password_reset_controller");
+const PasswordResetController = () => import('#controllers/auth/password_reset_controller')
 router
-  .get("auth/password/forgot", [PasswordResetController, "forgot"])
+  .get('auth/password/forgot', [PasswordResetController, 'forgot'])
   .use(middleware.guest())
-  .as("auth.password.forgot");
+  .as('auth.password.forgot')
 router
-  .post("auth/password/forgot", [PasswordResetController, "send"])
+  .post('auth/password/forgot', [PasswordResetController, 'send'])
   .use(middleware.guest())
-  .as("auth.password.send");
+  .as('auth.password.send')
 router
-  .get("auth/password/reset", [PasswordResetController, "reset"])
+  .get('auth/password/reset', [PasswordResetController, 'reset'])
   .use(middleware.guest())
-  .as("auth.password.reset");
+  .as('auth.password.reset')
 router
-  .post("auth/password/reset", [PasswordResetController, "store"])
+  .post('auth/password/reset', [PasswordResetController, 'store'])
   .use(middleware.guest())
-  .as("auth.password.store");
+  .as('auth.password.store')
 
-const EmailVerificationController = () =>
-  import("#controllers/auth/email_verification_controller");
-router
-  .get("auth/email/verify", [EmailVerificationController, "verify"])
-  .as("auth.email.verify");
+const EmailVerificationController = () => import('#controllers/auth/email_verification_controller')
+router.get('auth/email/verify', [EmailVerificationController, 'verify']).as('auth.email.verify')
 
-const MatricesController = () => import("#controllers/matrices_controller");
+const MatricesController = () => import('#controllers/matrices_controller')
 router
-  .get("matrices/:id/render", [MatricesController, "render"])
-  .as("matrices.id.render")
-  .use(middleware.auth());
+  .get('matrices/:id/render', [MatricesController, 'render'])
+  .as('matrices.id.render')
+  .use(middleware.auth())
 router
-  .get("matrices/render", [MatricesController, "renderToken"])
-  .as("matrices.render")
-  .use(middleware.guest());
-router
-  .resource("matrices", MatricesController)
-  .use("*", middleware.auth())
-  .as("matrices");
+  .get('matrices/render', [MatricesController, 'renderToken'])
+  .as('matrices.render')
+  .use(middleware.guest())
+router.resource('matrices', MatricesController).use('*', middleware.auth()).as('matrices')
