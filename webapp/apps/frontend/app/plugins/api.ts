c509eb09532b84870babc803ea1052cb3d5147d8
Bryan <bryan.prolong@outlook.fr>
2026-05-09 18:14:10 +0200

    feat(webapp.frontend): redirect 401 request to login page

--- webapp/apps/frontend/app/plugins/api.ts
+++ webapp/apps/frontend/app/plugins/api.ts
@@ -9,6 +9,17 @@ export default defineNuxtPlugin(() => {
     baseUrl: config.public.apiUrl || 'http://localhost:3333',
     registry,
     plugins: [superjson()],
+    hooks: {
+      beforeError: [
+        async (error) => {
+          if (error.response.status === 401) {
+            await navigateTo('/login')
+          }
+
+          return error
+        },
+      ],
+    },
   })
 
   return {
